7f8a617632daeb4b1a0a7f33f8dea3b044fbccfe
hickey <hickey_lee@outlook.com>
2022-10-31 07:37:59 +0800

    regular edit

---
+++ src/main/java/leetcode/Grammar.java
@@ -0,0 +1,27 @@
+package leetcode;
+import com.esotericsoftware.minlog.Log;
+import leetcode.MaxValueinList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grammar {
+    public static void main(String[] args) throws Exception {
+
+//        List arrayList = new ArrayList<String>();
+//        即为泛型，帮助在编译的时候报错
+        List<String> arrayList = new ArrayList<String>();
+
+//        List arrayList = new ArrayList();
+
+        arrayList.add("aaaa");
+        arrayList.add("bbb");
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            String item = (String) arrayList.get(i);
+            System.out.println("item = " + item);
+        }
+
+    }
+}
+
--- src/main/java/leetcode/Main.java
+++ src/main/java/leetcode/Main.java
@@ -3,11 +3,12 @@ import leetcode.MaxValueinList;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        System.out.println("Hello Flink");
-
-
-
-
+        MaxValueinList d = new MaxValueinList();
+        int[] num = {1,22,11,5,8,15,6,7,34,9};
+        int g = MaxValueinList.findKthLargest(num, 5);
+        System.out.println(g);
 
     }
 }
+
+
